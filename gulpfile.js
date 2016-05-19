29d37c0982caccd1194eee50d187dff791409efb
SIRO DIAZ PALAZON <siro.diaz1@um.es>
2016-05-19 18:30:35 +0200

    Gulp task and first plugin upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,16 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    uglify = require('gulp-uglify');
+    
+gulp.task('jsmin', function() {
+    return gulp.src('src/jquery.scroll-animate.js')
+        .pipe(plumber())
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('src/jquery.scroll-animate.js', ['jsmin']);
+});
+
+gulp.task('default', ['watch']);
